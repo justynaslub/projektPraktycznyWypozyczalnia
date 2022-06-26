cd2dafa94a4c7f960cfaa5c9147297675cbb9e54
Przemek <przemyslaw.malkowski7@gmail.com>
2022-06-26 12:18:41 +0200

    Stworzony entityManager i entityManagerFactory

---
+++ src/main/java/carRental/Customers.java
@@ -0,0 +1,28 @@
+package carRental;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.Entity;
+import javax.persistence.Table;
+import java.util.Date;
+
+@AllArgsConstructor
+@Getter
+@Setter
+@Entity
+@Table(name = "Customers")
+
+public class Customers {
+
+    private String id;
+    private String customer_password;
+    private String first_name;
+    private String last_name;
+    private Date birth_date;
+    private String phone_number;
+    private String customer_email;
+    private int licence;
+
+}
--- src/main/java/carRental/Main.java
+++ src/main/java/carRental/Main.java
@@ -1,14 +1,23 @@
 package carRental;
 
+import javax.persistence.Entity;
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class Main {
 
+    private static EntityManagerFactory entityManagerFactory;
+    private static EntityManager entityManager;
+    private static CustomersRepository customersRepository;
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws SQLException {
 
+    entityManagerFactory = Persistence.createEntityManagerFactory("mysql-project_rents");
+    entityManager = entityManagerFactory.createEntityManager();
 
 
         try (var connection = DriverManager.getConnection ("jdbc:mysql://localhost:3306/project_rents", "admin", "Password1");){
