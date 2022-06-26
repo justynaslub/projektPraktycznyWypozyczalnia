4179521ac1bf1081fa400093a89b241d75e940a8
Przemek <przemyslaw.malkowski7@gmail.com>
2022-06-26 08:45:19 +0200

    1. Usunięcie duplikatów z pom.xml 2. Połączenie z bazą danych project_rents

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,4 +1,23 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class Main {
+
+
+    public static void main(String[] args) {
+
+
+
+        try (var connection = DriverManager.getConnection ("jdbc:mysql://localhost:3306/project_rents", "admin", "Password1");){
+            System.out.println("Sucessfully connected to database.");
+
+        } catch (SQLException e) {
+            System.out.println("Something went wrong.");
+        }
+
+
+    }
 
 
 
