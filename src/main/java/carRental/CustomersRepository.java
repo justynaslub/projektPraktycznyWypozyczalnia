549bb33ae6af87f34967c566c0b9764a1d68d5e4
Przemek <przemyslaw.malkowski7@gmail.com>
2022-06-26 11:46:09 +0200

    Stworzony customers repo

---
+++ src/main/java/carRental/CustomersRepository.java
@@ -0,0 +1,14 @@
+package carRental;
+
+import java.sql.SQLException;
+
+public interface CustomersRepository {
+
+    void deleteCustomerByIdAndPassword (String id, String customer_password) throws SQLException;
+
+    void updateFirstName (String first_name) throws SQLException;
+    void updateFirstAndLastName (String last_name) throws SQLException;
+    void updatePassword (String customer_password) throws SQLException;
+    void updateLicence (int licence) throws SQLException;
+    void updatePhoneNumber (int phone_number) throws SQLException;
+}
